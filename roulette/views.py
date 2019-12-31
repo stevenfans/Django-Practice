d7d2bff20fd76b2e03354b68a57b8275939e2d18
Steven Phan <steven.phan009@gmail.com>
2019-12-30 23:04:59 -0800

    added new template

--- roulette/views.py
+++ roulette/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 #always pass in request
 def index(request):
-    return HttpResponse("<h1>This is the page for the roullete main page</h1>")
+    template = loader.get_template('roulette/index.html')
+    context = {
+        
+    }
+   #return HttpResponse("<h1>This is the page for the roullete main page</h1>")
+    return HttpResponse(template.render(context,request))
