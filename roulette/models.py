4ce62c1036c81673d67a5ed56f35f71be91a8655
Steven Phan <steven.phan009@gmail.com>
2019-12-30 21:53:28 -0800

    created first database, and activated models

--- roulette/models.py
+++ roulette/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    longitude = models.DecimalField(max_digits=999, decimal_places=3)
+    latitude = models.DecimalField(max_digits=999, decimal_places=3)
