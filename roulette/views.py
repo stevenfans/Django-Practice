f362c8a5bcc782351fa5c515972c17eb58d87fbe
Steven Phan <steven.phan009@gmail.com>
2020-01-22 10:05:24 -0800

    need to add js

--- roulette/views.py
+++ roulette/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_list_or_404
+from rest_framework.views import APIView
+from rest_framework import status
+
+
 
 # Create your views here.
-#need to work on views and add css
+
 #always pass in request
 def index(request):
     template = loader.get_template('roulette/index.html')
     context = {
         
     }
+
    #return HttpResponse("<h1>This is the page for the roullete main page</h1>")
     return HttpResponse(template.render(context,request))
