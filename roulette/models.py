1b399c93763246747bafb2e78a5f6ee1a6463d9b
Steven Phan <steven.phan009@gmail.com>
2020-01-23 23:37:32 -0800

    tested coordinates to the database

--- roulette/models.py
+++ roulette/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
+def printOut(coord):
+    return coord 
 
 class Location(models.Model):
     longitude = models.DecimalField(max_digits=999, decimal_places=6)
     latitude = models.DecimalField(max_digits=999, decimal_places=6)
+
+    printOut(longitude)
+
+
--- roulette/views.py
+++ roulette/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import get_list_or_404
+from roulette.models import Location 
 
 # from rest_framework.views import APIView
 # from rest_framework import status
@@ -13,7 +14,6 @@ def index(request):
     template = loader.get_template('roulette/index.html')
     # lat = float(request.GET.get('lat'))
     context = {}
-   #return HttpResponse("<h1>This is the page for the roullete main page</h1>")
     return HttpResponse(template.render(context,request))
 
 def process_loc(request):
@@ -23,6 +23,11 @@ def process_loc(request):
     lon = float(request.GET.get('lon'))
     print('latitdue is', lat)
     print('longitude is', lon)
-    print("FUCK ME, Why do you not work????")
+    location = Location(latitude = lat, longitude = lon) 
+    location.save()
+    # print(location.id)
+    print(Location.objects.all())
     return HttpResponse(template.render(context,request))
-    # return template.render()
+
+def testprint():
+    test = Location
