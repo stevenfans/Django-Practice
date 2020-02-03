9c81793919940e5ab7baa57113ce1f0ef1b1b8df
Steven Phan <steven.phan009@gmail.com>
2020-02-03 10:12:21 -0800

    button clicks and is able to repeatedly get new location

--- roulette/static/js/spinning.js
+++ roulette/static/js/spinning.js
@@ -1,10 +1,15 @@
 // this script just spins the wheel on spin click
 var spin = document.getElementById("buttonStr")
-
+var degrees = 20; // start off with 0 degrees
 // $("button").click(function(){
 //     alert("help i've fallen and i cant get up"); 
 //   }); 
 
-$("buttonStr").click(function(){
+$("#buttonStr").click(function rotationAnimation(object, speed){
+    var objectElement = document.getElementById(object); 
+    objectElement.style.transform = "rotate("+degrees+"deg)"; 
+    objectElement.style.webkitTransform = "rotate("+degrees+"deg)"; 
+    objectElement.style.msTransform = "rotate("+degrees+"deg)";
+
     alert("test"); 
-})
+});  
