b20c0e96fdf378b23ab7aa5e75253f4cdb8f98f4
Steven Phan <steven.phan009@gmail.com>
2020-02-03 20:38:18 -0800

    added spinning animation

--- roulette/static/js/spinning.js
+++ roulette/static/js/spinning.js
@@ -1,15 +1,34 @@
-// this script just spins the wheel on spin click
-var spin = document.getElementById("buttonStr")
-var degrees = 20; // start off with 0 degrees
-// $("button").click(function(){
-//     alert("help i've fallen and i cant get up"); 
-//   }); 
+    var loop; 
+    var degrees = 1; // start off with 0 degrees
 
-$("#buttonStr").click(function rotationAnimation(object, speed){
-    var objectElement = document.getElementById(object); 
-    objectElement.style.transform = "rotate("+degrees+"deg)"; 
-    objectElement.style.webkitTransform = "rotate("+degrees+"deg)"; 
-    objectElement.style.msTransform = "rotate("+degrees+"deg)";
+    $(document).ready(function(){
+        // this script just spins the wheel on spin click
+        var wheel = document.getElementById("wheel"); 
+        //TODO: Uncaught RangeError: Maximum call stack size exceeded
+        $("#buttonStr").click(function(){
+            console.log("inside function"); 
+            function rotationAnimation(){
+                wheel.style.transform = "rotate("+degrees+"deg)"; 
+                wheel.style.webkitTransform = "rotate("+degrees+"deg)"; 
+                wheel.style.msTransform = "rotate("+degrees+"deg)"; 
+                
+                loop = setTimeout(rotationAnimation(),20);
 
-    alert("test"); 
-});  
+                degrees++; 
+                if(degrees>359){
+                    degrees == 1; 
+                }
+            }
+            console.log("outsied function");
+            rotationAnimation();
+    });  
+})
+console.log("test that spinning is connected");
+
+// $(document).ready(function(){
+//     $('#buttonStr').on('click',function(){
+//         $('#wheel').animate({'-webkit-animation':'rotation 0.4s linear 3',
+//                             'rotation': })
+//         alert("FUCK ME");
+//     });
+// });
