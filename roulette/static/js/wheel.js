93788ca2bd2853b518d88a61b7d05affc6f7aafb
Steven Phan <steven.phan009@gmail.com>
2020-02-04 19:40:46 -0800

    got rotation to finally work

--- roulette/static/js/wheel.js
+++ roulette/static/js/wheel.js
@@ -1,34 +1,26 @@
     var loop; 
     var degrees = 1; // start off with 0 degrees
 
-    $(document).ready(function(){
+    // $(document).ready(function(){
         // this script just spins the wheel on spin click
-        var wheel = document.getElementById("wheel"); 
         //TODO: Uncaught RangeError: Maximum call stack size exceeded
         $("#buttonStr").click(function(){
+            var wheel = document.getElementById("wheel"); 
             console.log("inside function"); 
             function rotationAnimation(){
                 wheel.style.transform = "rotate("+degrees+"deg)"; 
                 wheel.style.webkitTransform = "rotate("+degrees+"deg)"; 
                 wheel.style.msTransform = "rotate("+degrees+"deg)"; 
                 
-                loop = setTimeout(rotationAnimation(),20);
+                setTimeout(rotationAnimation,20);
 
                 degrees++; 
                 if(degrees>359){
                     degrees == 1; 
                 }
             }
-            console.log("outsied function");
+            console.log("outside function");
             rotationAnimation();
     });  
-})
+// })
 console.log("test that spinning is connected");
-
-// $(document).ready(function(){
-//     $('#buttonStr').on('click',function(){
-//         $('#wheel').animate({'-webkit-animation':'rotation 0.4s linear 3',
-//                             'rotation': })
-//         alert("FUCK ME");
-//     });
-// });
