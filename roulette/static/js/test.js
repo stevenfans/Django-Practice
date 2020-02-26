a1383d9004a501e6b3a322ba5a2a45d6d8225e2c
Steven Phan <steven.phan009@gmail.com>
2020-02-25 20:16:30 -0800

    url change on window.location.href

--- roulette/static/js/test.js
+++ roulette/static/js/test.js
@@ -1,13 +1,14 @@
 var spinButton = document.getElementById("buttonRel"); 
 console.log("test"); 
 spinButton.onclick = function(){
-    // alert("you clicked the button"); 
+     alert("you clicked the button"); 
 
     $.ajax({
         type:"GET",
-        url:"reload",
+        // url:"reload",
         success:function(){
-            console.log("test reload was a success")
+            console.log("test reload was a success");
+            window.location.href="http://127.0.0.1:8000/results/";
         },
         // error:function(xhr,errmsg,err){
         //     console.log("test was a failure")
