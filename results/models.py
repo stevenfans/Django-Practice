d26ab1d5af8455d6f5c9f72bfac5ba549c85e104
Steven Phan <steven.phan009@gmail.com>
2020-02-27 15:18:26 -0800

    added mysql

--- results/models.py
+++ results/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Restauraunt(models.Model):
+    name = models.CharField(max_length=30)
+    rating = models.DecimalField(max_digits=999, decimal_places=3)
+    phone_num = models.CharField(max_length=10)
+    price = models.CharField(max_length=10)
+
+    # string representation of model
+    def __str__(self):
+        return self.title
--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,8 +78,15 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME':'sample',
+        'USER':'root',
+        # 'PASSWORD':'',
+        # 'HOST':'127.0.0.1',
+        # 'PORT':'33',
+
     }
 }
 
